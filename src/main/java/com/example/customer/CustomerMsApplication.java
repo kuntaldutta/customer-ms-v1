edb2fbafbbff89653d6194f6d6a08dddadafc0ea
kuntdutt <kuntdutt@cisco.com>
2021-05-14 16:37:03 +0530

    renamed the package

--- src/main/java/com/example/customer/CustomerMsApplication.java
+++ src/main/java/com/example/customer/CustomerMsApplication.java
@@ -1,4 +1,4 @@
-package com.example.customerms;
+package com.example.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
